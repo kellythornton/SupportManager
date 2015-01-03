3eff98739dd3bf940ba3703e90fc358f9ae5d65e
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-01-02 17:57:47 -0700

    Update #3
    Update #3

---
+++ SupportManager/Model/Employee/Employee-events.js
@@ -0,0 +1,1 @@
+﻿model.Employee.fullName.onGet = function() {	return this.firstName + " " + this.lastName;};
---
+++ SupportManager/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,43 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var btnEmployees = {};	// @button
+	var btnTickets = {};	// @button
+	var btnProjects = {};	// @button
+	var btnHome = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	btnEmployees.click = function btnEmployees_click (event)// @startlock
+	{// @endlock
+		document.getElementById('ifMain').src="/employees.waPage/";
+	};// @lock
+
+	btnTickets.click = function btnTickets_click (event)// @startlock
+	{// @endlock
+		document.getElementById('ifMain').src="/tickets.waPage/";
+	};// @lock
+
+	btnProjects.click = function btnProjects_click (event)// @startlock
+	{// @endlock
+		document.getElementById('ifMain').src="/projects.waPage/";
+	};// @lock
+
+	btnHome.click = function btnHome_click (event)// @startlock
+	{// @endlock
+		document.getElementById('ifMain').src="/home.waPage/";
+	};// @lock
+
+// Assign appropriate menu items to display the correct page in the mainFrame frame widget.
+	
+	
+
+// @region eventManager// @startlock
+	WAF.addListener("btnEmployees", "click", btnEmployees.click, "WAF");
+	WAF.addListener("btnTickets", "click", btnTickets.click, "WAF");
+	WAF.addListener("btnProjects", "click", btnProjects.click, "WAF");
+	WAF.addListener("btnHome", "click", btnHome.click, "WAF");
+// @endregion
+};// @endlock
